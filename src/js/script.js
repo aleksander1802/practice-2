3d2636fd22503951f408edb9e9a9b99518f68bf3
Alex2 <aleksander1802@gmail.com>
2022-07-12 13:41:56 +0300

    init: section prengi

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,19 @@ $(document).ready(function(){
       
     });
   });
+
+
+
+
+
+  // function solution(number) {
+  //   let arr = new Array(number).fill(number).map((_, x) => x + 1);
+  //   arr.length = arr.length - 1
+  //  return arr.length < 3 ? 0 : arr.filter(x => (x % 3 === 0) || (x % 5 === 0)).reduce((x, y) => x + y)
+    
+    
+
+    
+  // }
+
+  // console.log(solution(10));
