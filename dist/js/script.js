57a0b937889a5f81afc70d22db818dcdf6b04172
Alex2 <aleksander1802@gmail.com>
2022-07-14 22:17:56 +0300

    decision

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,7 +3,10 @@ $(document).ready(function(){
       arrows: true,
       dots: true,
       draggable: false,
-      
+    });
+    $('.decision__slider').slick({
+      arrows: true,
+      dots: true
     });
   });
 
@@ -11,14 +14,9 @@ $(document).ready(function(){
 
 
 
-  // function solution(number) {
-  //   let arr = new Array(number).fill(number).map((_, x) => x + 1);
-  //   arr.length = arr.length - 1
-  //  return arr.length < 3 ? 0 : arr.filter(x => (x % 3 === 0) || (x % 5 === 0)).reduce((x, y) => x + y)
-    
-    
 
-    
-  // }
 
-  // console.log(solution(10));
+
+
+
+  
--- src/js/script.js
+++ src/js/script.js
@@ -3,25 +3,116 @@ $(document).ready(function(){
       arrows: true,
       dots: true,
       draggable: false,
-      
     });
+    
   });
 
 
 
 
 
-  // function solution(number) {
-  //   let arr = new Array(number).fill(number).map((_, x) => x + 1);
-  //   arr.length = arr.length - 1
-  //  return arr.length < 3 ? 0 : arr.filter(x => (x % 3 === 0) || (x % 5 === 0)).reduce((x, y) => x + y)
-    
-    
 
-    
-  // }
 
-  // console.log(solution(10));
+  const slides = document.querySelectorAll('.decision__slider_list-item'),
+        slider = document.querySelector('.decision__slider'),
+        prev = document.querySelector('.decision__slider-prev'),
+        next = document.querySelector('.decision__slider-next'),
+        slidesWrapper = document.querySelector('.decision__slider_list'),
+        slidesField = document.querySelector('.decision__slider_inner'),
+        width = window.getComputedStyle(slidesWrapper).width,
+        dots = document.querySelectorAll('.decision__slider_control-list-slide'),
+        sliderList = document.querySelectorAll('.decision__slider_list-item');
+
+
+        let slideIndex = 1;
+        let offset = 0;
+        let selectedLI;  
+
+
+        function highlight(td, arrow) {
+          
+          if (selectedLI) { 
+            selectedLI.classList.remove('active');
+          }
+          selectedLI = td;
+          selectedLI.classList.add('active'); 
+        }
+        
+
+        next.addEventListener('click', () => {
+          
+          if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+            offset = 0;
+          } else {
+            offset += +width.slice(0, width.length - 2)
+          }
+          slidesField.style.transform = `translateX(-${offset}px)`;
+          console.log(sliderList);
+        });
+
+
+
+        prev.addEventListener('click', () => {
+          if (offset == 0) {
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1)
+          } else {
+            offset -= +width.slice(0, width.length - 2)
+          }
+          slidesField.style.transform = `translateX(-${offset}px)`
+          
+        })
+
+        
+
+        slidesField.style.width = 100 * slides.length + '%';
+
+        slidesField.style.display = 'flex';
+        slidesField.style.transition = '0.5s all';
+
+        slidesWrapper.style.overflow = 'hidden'
+
+
+        slides.forEach(slide  => {
+          slide.style.width = width;
+        });
+
+
+        
+        dots.forEach(item => {
+          
+          item.addEventListener('click', (e) => {
+            let target = e.target;
+            
+            const slideTo = target.getAttribute('data-slide-to');
+
+            slideIndex = slideTo;
+
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+            slidesField.style.transform = `translateX(-${offset}px)`;
+            
+            if (target.tagName != 'LI') return; // не на TD? тогда не интересует
+
+            highlight(target);
+            console.log(target.getAttribute('data-slide-to'));
+          })
+
+          
+        })
+
+        
+      
+        
+        
+        
+
+ 
+
+
+
+
+
+
 
 
 
